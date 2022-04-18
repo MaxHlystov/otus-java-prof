2f847a91504575927af04912de13678a4a75190f
Khlystov Maksim <maxvls@gmail.com>
2022-04-18 14:47:39 +0300

    Hw08 Optimize working of Summator class

--- hw08-gc/src/main/java/ru/calculator/Summator.java
+++ hw08-gc/src/main/java/ru/calculator/Summator.java
@@ -4,29 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Summator {
-    private Integer sum = 0;
-    private Integer prevValue = 0;
-    private Integer prevPrevValue = 0;
-    private Integer sumLastThreeValues = 0;
-    private Integer someValue = 0;
-    private final List<Data> listValues = new ArrayList<>();
+    private int valuesCount = 0;
+    private int sum = 0;
+    private int prevValue = 0;
+    private int prevPrevValue = 0;
+    private int sumLastThreeValues = 0;
+    private int someValue = 0;
 
     //!!! сигнатуру метода менять нельзя
     public void calc(Data data) {
-        listValues.add(data);
-        if (listValues.size() % 6_600_000 == 0) {
-            listValues.clear();
+        valuesCount++;
+        if (valuesCount == 6_600_000) {
+            valuesCount = 0;
         }
-        sum += data.getValue();
 
-        sumLastThreeValues = data.getValue() + prevValue + prevPrevValue;
+        final int currentValue = data.getValue();
+        sum += currentValue;
+
+        sumLastThreeValues = currentValue + prevValue + prevPrevValue;
 
         prevPrevValue = prevValue;
-        prevValue = data.getValue();
+        prevValue = currentValue;
 
         for (var idx = 0; idx < 3; idx++) {
-            someValue += (sumLastThreeValues * sumLastThreeValues / (data.getValue() + 1) - sum);
-            someValue = Math.abs(someValue) + listValues.size();
+            someValue += (sumLastThreeValues * sumLastThreeValues / (currentValue + 1) - sum);
+            someValue = Math.abs(someValue) + valuesCount;
         }
     }
 
