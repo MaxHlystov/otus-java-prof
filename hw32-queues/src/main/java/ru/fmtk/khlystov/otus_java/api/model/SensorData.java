9d7cefe462fd2ec85a935ef4d66fc4e9c4d15924
Khlystov Maksim <maxvls@gmail.com>
2022-05-31 16:43:29 +0300

    Hw32 Got SensorDataProcessorBuffered to work

--- hw32-queues/src/main/java/ru/fmtk/khlystov/otus_java/api/model/SensorData.java
+++ hw32-queues/src/main/java/ru/fmtk/khlystov/otus_java/api/model/SensorData.java
@@ -2,8 +2,10 @@ package ru.fmtk.khlystov.otus_java.api.model;
 
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class SensorData {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
     private final LocalDateTime measurementTime;
     private final String room;
     private final Double value;
@@ -24,5 +26,15 @@ public class SensorData {
 
     public Double getValue() {
         return value;
+    }
+
+    @Override
+    public String toString() {
+        String at = measurementTime == null ? "" : "at=" + measurementTime.format(formatter);
+        return "SensorData{" +
+                at +
+                ", room='" + room + '\'' +
+                ", value=" + value +
+                '}';
     }
 }
--- hw32-queues/src/main/java/ru/fmtk/khlystov/otus_java/services/processors/SensorDataProcessorBuffered.java
+++ hw32-queues/src/main/java/ru/fmtk/khlystov/otus_java/services/processors/SensorDataProcessorBuffered.java
@@ -1,5 +1,11 @@
 package ru.fmtk.khlystov.otus_java.services.processors;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.fmtk.khlystov.otus_java.api.SensorDataProcessor;
@@ -12,26 +18,38 @@ public class SensorDataProcessorBuffered implements SensorDataProcessor {
 
     private final int bufferSize;
     private final SensorDataBufferedWriter writer;
+    private final List<SensorData> dataBuffer;
 
     public SensorDataProcessorBuffered(int bufferSize, SensorDataBufferedWriter writer) {
         this.bufferSize = bufferSize;
         this.writer = writer;
+        this.dataBuffer = new ArrayList<>(bufferSize);
     }
 
     @Override
     public void process(SensorData data) {
-    /*
-        if (dataBuffer.size() >= bufferSize) {
-            flush();
+        synchronized (dataBuffer) {
+            if (dataBuffer.size() >= bufferSize) {
+                flush();
+            }
+            this.dataBuffer.add(data);
         }
-    */
     }
 
     public void flush() {
-        try {
-            //writer.writeBufferedData(bufferedData);
-        } catch (Exception e) {
-            log.error("Ошибка в процессе записи буфера", e);
+        synchronized (dataBuffer) {
+            if (dataBuffer.size() > 0) {
+                try {
+                    SensorData[] array = dataBuffer.toArray(SensorData[]::new);
+                    dataBuffer.clear();
+                    Arrays.sort(array, Comparator.comparing(SensorData::getMeasurementTime));
+                    writer.writeBufferedData(Arrays.stream(array)
+                            .collect(Collectors.toCollection(ArrayList<SensorData>::new)));
+                } catch (Exception e) {
+                    log.error("Ошибка в процессе записи буфера", e);
+                    throw new RuntimeException(e);
+                }
+            }
         }
     }
 
